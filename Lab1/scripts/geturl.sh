5a0a2f2d39b0c231ac361565d195ca62a0f38e42
Anubhav Sharma <shaanubh@amazon.com>
2021-11-02 20:47:33 -0400

    added script to get URLs

---
+++ Lab1/scripts/geturl.sh
@@ -0,0 +1,4 @@
+stackname=$1
+
+APP_SITE_URL=$(aws cloudformation describe-stacks --stack-name $stackname --query "Stacks[0].Outputs[?OutputKey=='ApplicationSite'].OutputValue" --output text)
+echo "Application site URL: https://$APP_SITE_URL"
---
+++ Lab6/scripts/geturl.sh
@@ -0,0 +1,7 @@
+ADMIN_SITE_URL=$(aws cloudformation describe-stacks --stack-name serverless-saas --query "Stacks[0].Outputs[?OutputKey=='AdminAppSite'].OutputValue" --output text)
+LANDING_APP_SITE_URL=$(aws cloudformation describe-stacks --stack-name serverless-saas --query "Stacks[0].Outputs[?OutputKey=='LandingApplicationSite'].OutputValue" --output text)
+APP_SITE_URL=$(aws cloudformation describe-stacks --stack-name serverless-saas --query "Stacks[0].Outputs[?OutputKey=='ApplicationSite'].OutputValue" --output text)
+
+echo "Admin site URL: https://$ADMIN_SITE_URL"
+echo "Landing site URL: https://$LANDING_APP_SITE_URL"
+echo "App site URL: https://$APP_SITE_URL"
