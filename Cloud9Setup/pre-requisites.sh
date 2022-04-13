252c84e76085dee693951eb2c5e1e4498913c8ae
ujwbukka <ujwbukka@amazon.com>
2022-04-13 19:08:27 -0400

    Fixed typo error

--- Cloud9Setup/pre-requisites.sh
+++ Cloud9Setup/pre-requisites.sh
@@ -5,7 +5,7 @@
 sudo yum install -y amazon-linux-extras
 sudo amazon-linux-extras enable python3.8
 sudo yum install -y python3.8
-sudo alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8 1
+sudo alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8
 sudo alternatives --set python3 /usr/bin/python3.8
 
 # Uninstall aws cli v1 and Install aws cli version-2.3.0
