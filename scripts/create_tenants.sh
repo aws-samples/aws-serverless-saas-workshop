8406fbe5e92cfbb4e91089e7908efe6eef0f69f7
Syed Hussain <suhussai@ualberta.ca>
2022-06-29 11:57:16 -0600

    update create tenants script to automatically get url for saas admin api

--- scripts/create_tenants.sh
+++ scripts/create_tenants.sh
@@ -2,12 +2,50 @@
 ##
 ## This script is to help create different kinds of tenants quickly.
 ##
+## To use:
+## bash create_tenants.sh myemail mydomain.com
+##
 
-SAAS_ADMIN_URL="" # ex. https://m6slpkzugb.execute-api.us-west-2.amazonaws.com
-EMAIL_ALIAS="" # ex. test
-EMAIL_DOMAIN="" # ex. test.com
+EMAIL_ALIAS="$1" # ex. test
+EMAIL_DOMAIN="$2" # ex. test.com
 
-echo "Creating a Standard tenant..."
+echo "$(date) finding saas admin API GW url..."
+next_token=""
+while true; do
+    if [[ "${next_token}" == "" ]]; then
+        echo "$(date) making api call to search for saas admin API GWs..."
+        response=$(aws apigateway get-rest-apis --max-items 1)
+    else
+        echo "$(date) making api call to search for saas admin API GWs..."
+        response=$(aws apigateway get-rest-apis --max-items 1 --starting-token "$next_token")
+    fi
+
+    api_gw_id=$(echo "$response" | jq -r '.items[] | select (.name | match("serverless-saas-admin-api")) | .id')
+    if [[ "${api_gw_id}" != "" ]]; then
+        echo "$(date) saas admin API rest id found!"
+        # no need to look any further...
+        break
+    fi
+
+    next_token=$(echo "$response" | jq '.NextToken')
+    if [[ "${next_token}" == "null" ]]; then
+        echo "$(date) no more API GWs left!"
+        # no more results left. Exit loop...
+        break
+    fi
+done
+
+SAAS_ADMIN_URL_STAGE="prod"
+CURRENT_REGION=$(aws configure get region)
+SAAS_ADMIN_URL="https://${api_gw_id}.execute-api.${CURRENT_REGION}.amazonaws.com/${SAAS_ADMIN_URL_STAGE}" # ex. https://m6slpkzugb.execute-api.us-west-2.amazonaws.com
+
+echo "EMAIL_ALIAS=${EMAIL_ALIAS}"
+echo "EMAIL_DOMAIN=${EMAIL_DOMAIN}"
+echo "SAAS_ADMIN_URL=${SAAS_ADMIN_URL}"
+
+read -p "press any key to confirm above parameters and continue..."
+
+echo "$(date) Creating a Standard tenant..."
 curl --location --request POST "${SAAS_ADMIN_URL}/prod/registration" \
     --header 'Content-Type: application/json' \
     --data-raw "{
@@ -17,9 +55,9 @@ curl --location --request POST "${SAAS_ADMIN_URL}/prod/registration" \
     \"tenantPhone\": \"1234567890\",
     \"tenantTier\": \"Standard\"
 }"
-echo "Done creating a Standard tenant!"
+echo "$(date) Done creating a Standard tenant!"
 
-echo "Creating a Platinum tenant..."
+echo "$(date) Creating a Platinum tenant..."
 curl --location --request POST "${SAAS_ADMIN_URL}/prod/registration" \
     --header 'Content-Type: application/json' \
     --data-raw "{
@@ -29,9 +67,9 @@ curl --location --request POST "${SAAS_ADMIN_URL}/prod/registration" \
     \"tenantPhone\": \"1234567890\",
     \"tenantTier\": \"Platinum\"
 }"
-echo "Done creating a Platinum tenant!"
+echo "$(date) Done creating a Platinum tenant!"
 
-echo "Creating a Premium tenant..."
+echo "$(date) Creating a Premium tenant..."
 curl --location --request POST "${SAAS_ADMIN_URL}/prod/registration" \
     --header 'Content-Type: application/json' \
     --data-raw "{
@@ -41,4 +79,4 @@ curl --location --request POST "${SAAS_ADMIN_URL}/prod/registration" \
     \"tenantPhone\": \"1234567890\",
     \"tenantTier\": \"Premium\"
 }"
-echo "Done creating a Premium tenant!"
+echo "$(date) Done creating a Premium tenant!"
