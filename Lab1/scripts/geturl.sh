7656a2a99345ea3575409e213ecfc200b018f209
Syed Hussain <suhussai@ualberta.ca>
2022-08-11 20:02:41 -0600

    update scripts for lab1

--- Lab1/scripts/geturl.sh
+++ Lab1/scripts/geturl.sh
@@ -1,4 +1,5 @@
+#!/bin/bash
 stackname=$1
 
-APP_SITE_URL=$(aws cloudformation describe-stacks --stack-name $stackname --query "Stacks[0].Outputs[?OutputKey=='ApplicationSite'].OutputValue" --output text)
-echo "Application site URL: https://$APP_SITE_URL"
+APP_SITE_URL=$(aws cloudformation describe-stacks --stack-name "$stackname" --query "Stacks[0].Outputs[?OutputKey=='ApplicationSite'].OutputValue" --output text)
+echo "Application site URL: https://${APP_SITE_URL}"
