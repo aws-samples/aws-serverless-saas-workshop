a6297cd78ccd27995a3cbd442b5f779ef67a2ad9
Syed Hussain <suhussai@ualberta.ca>
2022-08-11 20:34:22 -0600

    update scripts for lab2

--- Lab2/scripts/geturl.sh
+++ Lab2/scripts/geturl.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 ADMIN_SITE_URL=$(aws cloudformation describe-stacks --stack-name serverless-saas --query "Stacks[0].Outputs[?OutputKey=='AdminAppSite'].OutputValue" --output text)
 LANDING_APP_SITE_URL=$(aws cloudformation describe-stacks --stack-name serverless-saas --query "Stacks[0].Outputs[?OutputKey=='LandingApplicationSite'].OutputValue" --output text)
 
