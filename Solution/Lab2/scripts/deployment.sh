152f7d2ae0e08ff581b0af2e5e4e84c9773a76a8
Syed Hussain <suhussai@ualberta.ca>
2022-08-23 14:29:07 -0600

    fix pylint issue in lab2 and lab2 solution (#29)

--- Solution/Lab2/scripts/deployment.sh
+++ Solution/Lab2/scripts/deployment.sh
@@ -74,7 +74,7 @@ if [[ $server -eq 1 ]]; then
   fi
 
   echo "Validating server code using pylint"
-  python3 -m pylint -E -d E0401 "$(find . -iname "*.py" -not -path "./.aws-sam/*")"
+  python3 -m pylint -E -d E0401 $(find . -iname "*.py" -not -path "./.aws-sam/*")
   if [[ $? -ne 0 ]]; then
     echo "****ERROR: Please fix above code errors and then rerun script!!****"
     exit 1
