15bfe6fb8a78fdf33d4edb574c73a94e024ba2af
Syed Hussain <suhussai@ualberta.ca>
2022-08-25 17:29:17 -0600

    fix application name and angular.json values

--- Lab3/client/Application/karma.conf.js
+++ Lab3/client/Application/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/dashboard'),
+      dir: require('path').join(__dirname, './coverage/application'),
       subdir: '.',
       reporters: [
         { type: 'html' },
