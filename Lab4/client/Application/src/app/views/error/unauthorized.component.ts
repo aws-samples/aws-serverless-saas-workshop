093dac79a24b4de902775b0b239771582df8ff0d
Syed Hussain <suhussai@ualberta.ca>
2022-09-01 21:12:50 -0600

    Lab UI copies (#32)
    * update lab3 landing and admin UI
    
    * update lab4 app, landing and admin UI
    
    * update lab5 landing and admin UI
    
    * update lab6 app, landing and admin UI

--- Lab4/client/Application/src/app/views/error/unauthorized.component.ts
+++ Lab4/client/Application/src/app/views/error/unauthorized.component.ts
@@ -15,37 +15,59 @@
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 import { Component, OnInit } from '@angular/core';
-import { OidcSecurityService } from 'angular-auth-oidc-client';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthConfigurationService } from './../auth/auth-configuration.service';
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-unauthorized',
   templateUrl: './unauthorized.component.html',
-  styleUrls: [ './unauthorized.component.scss'  ]
+  styleUrls: ['./unauthorized.component.scss'],
 })
 export class UnauthorizedComponent implements OnInit {
   tenantForm: FormGroup;
   params$: Observable<void>;
   error = false;
-  errorMessage : string;
+  errorMessage: string;
+  tenantNameRequired: boolean = false;
 
-  constructor(private oidcSecurityService: OidcSecurityService, private fb: FormBuilder,
+  constructor(
+    private fb: FormBuilder,
     private authConfigService: AuthConfigurationService,
-    private router: Router) {
-    this.tenantForm = this.fb.group({
-      tenantName: [null, [Validators.required]]
-    });
+    private _snackBar: MatSnackBar,
+    private router: Router
+  ) {
+    if (
+      environment.userPoolId &&
+      environment.appClientId &&
+      environment.apiGatewayUrl
+    ) {
+      // If a tenant's cognito configuration is provided in the
+      // "environment" object, then we take that instead of asking
+      // the visitor to provide the name of their tenant in order
+      // to do a look-up for that tenant's cognito configuration.
+      localStorage.setItem('tenantName', 'PooledTenants');
+      localStorage.setItem('userPoolId', environment.userPoolId);
+      localStorage.setItem('appClientId', environment.appClientId);
+      localStorage.setItem('apiGatewayUrl', environment.apiGatewayUrl);
+      this.tenantNameRequired = false;
+    }
   }
 
   ngOnInit(): void {
+    this.tenantForm = this.fb.group({
+      tenantName: [null, [Validators.required]],
+    });
   }
 
   isFieldInvalid(field: string) {
     const formField = this.tenantForm.get(field);
-    return formField.invalid && (formField.dirty || formField.touched);
+    return (
+      formField && formField.invalid && (formField.dirty || formField.touched)
+    );
   }
 
   displayFieldCss(field: string) {
@@ -55,33 +77,41 @@ export class UnauthorizedComponent implements OnInit {
   }
 
   hasRequiredError(field: string) {
-    return this.hasError(field, 'required');
+    return !!this.tenantForm.get(field)?.hasError('required');
   }
 
-  hasError(field: string, error: any) {
-    const formField = this.tenantForm.get(field);
-    return !!formField.errors[error];
+  openErrorMessageSnackBar(errorMessage: string) {
+    this._snackBar.open(errorMessage, 'Dismiss', {
+      duration: 4 * 1000, // seconds
+    });
   }
-
 
   login() {
+    if (!this.tenantNameRequired) {
+      this.router.navigate(['/dashboard']);
+      return true;
+    }
 
-    // let tenantName = this.tenantForm.value.tenantName;
-    // this.authConfigService.setTenantConfig(tenantName)
-    //  .then((val)=>{
-    //   this.oidcSecurityService.authorize();
+    let tenantName = this.tenantForm.value.tenantName;
+    if (!tenantName) {
+      this.errorMessage = 'No tenant name provided.';
+      this.error = true;
+      this.openErrorMessageSnackBar(this.errorMessage);
+      return false;
+    }
 
-    // }).catch((errorResponse) => {
-    //   this.error = true;
-    //   this.errorMessage = errorResponse.error.message;
-
-    // });
-
-    this.oidcSecurityService.authorize();
+    this.authConfigService
+      .setTenantConfig(tenantName)
+      .then((val) => {
+        this.router.navigate(['/dashboard']);
+      })
+      .catch((errorResponse) => {
+        this.error = true;
+        this.errorMessage =
+          errorResponse.error.message || 'An unexpected error occurred!';
+        this.openErrorMessageSnackBar(this.errorMessage);
+      });
 
     return false;
   }
-
-
-
 }
