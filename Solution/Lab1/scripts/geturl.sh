25d9c6b786884f0ff4818f3263c344934e3b002f
Ujwal Bukka <ujwbukka@amazon.com>
2022-11-09 11:46:52 -0500

    Update to get UI urls when running workshop in AWS event or in an AWS account

--- Solution/Lab1/scripts/geturl.sh
+++ Solution/Lab1/scripts/geturl.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-stackname=$1
+stackname="serverless-saas-workshop-lab1"
 
 APP_SITE_URL=$(aws cloudformation describe-stacks --stack-name "$stackname" --query "Stacks[0].Outputs[?OutputKey=='ApplicationSite'].OutputValue" --output text)
 echo "Application site URL: https://${APP_SITE_URL}"
