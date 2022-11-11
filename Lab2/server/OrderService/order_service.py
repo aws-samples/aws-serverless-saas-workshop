81c45a0b925259c78ce3fb6bd1cc549d027d8771
Sourav Paul <iamsouravin@gmail.com>
2022-11-11 14:34:16 +0000

    Fix product price TypeError for decimal values (#45)
    * Fix product price TypeError for decimal values
    
    * Fix float type unmarshalling for DDB
    
    Co-authored-by: Sourav Paul <psour@amazon.co.uk>

--- Lab2/server/OrderService/order_service.py
+++ Lab2/server/OrderService/order_service.py
@@ -5,6 +5,7 @@ import json
 import utils
 import logger
 import order_service_dal
+from decimal import Decimal
 from types import SimpleNamespace
 from aws_lambda_powertools import Tracer
 
@@ -20,14 +21,14 @@ def get_order(event, context):
     
 def create_order(event, context):  
     logger.info("Request received to create a order")
-    payload = json.loads(event['body'], object_hook=lambda d: SimpleNamespace(**d))
+    payload = json.loads(event['body'], object_hook=lambda d: SimpleNamespace(**d), parse_float=Decimal)
     order = order_service_dal.create_order(event, payload)
     logger.info("Request completed to create a order")
     return utils.generate_response(order)
     
 def update_order(event, context):    
     logger.info("Request received to update a order")
-    payload = json.loads(event['body'], object_hook=lambda d: SimpleNamespace(**d))
+    payload = json.loads(event['body'], object_hook=lambda d: SimpleNamespace(**d), parse_float=Decimal)
     params = event['pathParameters']
     orderId = params['id']
     order = order_service_dal.update_order(event, payload, orderId)
