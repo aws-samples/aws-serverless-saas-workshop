3a9baeb19d65f0b8da49a82af24ad812099365cd
Ujwal Bukka <ujwbukka@amazon.com>
2022-11-23 10:07:20 -0500

    Upgraded sam cli version to 1.64.0

--- Cloud9Setup/pre-requisites.sh
+++ Cloud9Setup/pre-requisites.sh
@@ -18,16 +18,16 @@ sudo ./aws/install
 rm awscliv2.zip
 rm -rf aws 
 
-# Install sam cli version 1.33.0
-echo "Installing sam cli version 1.33.0"
-wget https://github.com/aws/aws-sam-cli/releases/download/v1.33.0/aws-sam-cli-linux-x86_64.zip
+# Install sam cli version 1.64.0
+echo "Installing sam cli version 1.64.0"
+wget https://github.com/aws/aws-sam-cli/releases/download/v1.64.0/aws-sam-cli-linux-x86_64.zip
 unzip aws-sam-cli-linux-x86_64.zip -d sam-installation
 sudo ./sam-installation/install
 if [ $? -ne 0 ]; then
 	echo "Sam cli is already present, so deleting existing version"
 	sudo rm /usr/local/bin/sam
 	sudo rm -rf /usr/local/aws-sam-cli
-	echo "Now installing sam cli version 1.33.0"
+	echo "Now installing sam cli version 1.64.0"
 	sudo ./sam-installation/install    
 fi
 rm aws-sam-cli-linux-x86_64.zip
