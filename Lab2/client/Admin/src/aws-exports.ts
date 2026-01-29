aad349c30428420e243360b6bf23c0ceb00d7a54
Diego Riveros <lancdieg@amazon.com>
2026-01-29 11:52:00 -0500

    chore: Auto-commit before Lab6 deployment

--- Lab2/client/Admin/src/aws-exports.ts
+++ Lab2/client/Admin/src/aws-exports.ts
@@ -1,8 +1,8 @@
 const awsmobile = {
     "aws_project_region": "us-east-1",
     "aws_cognito_region": "us-east-1",
-    "aws_user_pools_id": "us-east-1_06hYXzvKX",
-    "aws_user_pools_web_client_id": "5orfbr0qm2863cc19usdq1il86",
+    "aws_user_pools_id": "us-east-1_0ZvpBVB4V",
+    "aws_user_pools_web_client_id": "3s2sjs4g1ia1h1qlh7ed543tm0",
 };
 
 export default awsmobile;
--- Lab2/client/Admin/src/environments/environment.ts
+++ Lab2/client/Admin/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://j25ah1ecfa.execute-api.us-east-1.amazonaws.com/prod',
+  apiUrl: 'https://5d2ckqtoq3.execute-api.us-east-1.amazonaws.com/prod',
 };
