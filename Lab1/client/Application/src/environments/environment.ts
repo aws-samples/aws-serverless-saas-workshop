2ae181788637f4beb9b50d76d034677b265d73df
Diego Riveros <lancdieg@amazon.com>
2026-01-31 22:10:00 -0500

    chore: Auto-commit before Lab6 deployment

--- Lab1/client/Application/src/environments/environment.ts
+++ Lab1/client/Application/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiGatewayUrl: 'https://cp8p9tnje0.execute-api.us-east-1.amazonaws.com/prod'
+  apiGatewayUrl: 'https://9pvnaxy260.execute-api.us-east-1.amazonaws.com/prod'
 };
