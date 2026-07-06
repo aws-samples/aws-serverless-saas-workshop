894bc29b1859481e8c671af21d347e362b9608f2
Alexander Vladimirov <55249162+avladi@users.noreply.github.com>
2026-07-06 13:35:56 -0700

    Update copyright year from 2022 to 2026 across all labs

--- Solution/Lab7/deployment.sh
+++ Solution/Lab7/deployment.sh
@@ -6,6 +6,6 @@ sam deploy --config-file samconfig.toml --region=$REGION
 CUR_BUCKET=$(aws cloudformation list-exports --query "Exports[?Name=='CURBucketname'].Value" --output text)
 AWSCURInitializerFunctionName=$(aws cloudformation list-exports --query "Exports[?Name=='AWSCURInitializerFunctionName'].Value" --output text)
 
-aws s3 cp SampleCUR/ s3://$CUR_BUCKET/curoutput/year=2022/month=10/ --recursive
+aws s3 cp SampleCUR/ s3://$CUR_BUCKET/curoutput/year=2026/month=10/ --recursive
 
 aws lambda invoke --function-name $AWSCURInitializerFunctionName lambdaoutput.json
